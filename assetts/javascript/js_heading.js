91d5ff7e88f62d09ace83c7a62a94e92a5ee8480
XuanThanh <thanh26092000@gmail.com>
2021-05-26 16:17:54 +0700

    first commit

---
+++ assetts/javascript/js_heading.js
@@ -0,0 +1,14 @@
+ 
+ 
+// const $ = document.querySelector.bind(document);
+// const $$ = document.querySelectorAll.bind(document);
+  
+//   const items = $$('.mid-header-item');
+
+// items.forEach((item) => {
+//     item.onmouseover = function() {
+//       $('.mid-header-item').classList.remove('mid-header-item-separite');
+//       this.classList.add('mid-header-item-separite');
+//     };
+//   });
+
